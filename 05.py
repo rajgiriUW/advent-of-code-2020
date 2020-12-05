5e8e2be904678f355628d14f8d0af99d9bf4ac8a
Rajiv Giridharagopal <rgiri@uw.edu>
2020-12-05 00:42:52 -0800

    Update 05.py

--- 05.py
+++ 05.py
@@ -5,13 +5,12 @@ Created on Sat Dec  5 00:06:18 2020
 @author: Raj
 """
 
+import numpy as np 
 
 base = r'C:/Users/Raj/OneDrive/UW Work/Coding and Signal Processing Work/Python/aoc_2020/'
 f = open(base + r'/05_seats.txt')
 
 data = f.read().split('\n')[:-1] # drop last empty row
-
-highest = 0
 
 def to_bin(line):
     
@@ -25,22 +24,17 @@ def to_bin(line):
         
     return ''.join(row), ''.join(col)
 
-for d in data:
-    
-    row, col = [int(k, 2) for k in to_bin(d)]
-    highest = max(highest, row*8 + col)
-
-print(highest)
-
-# part 2
-import numpy as np 
 def to_seat(d):
     
     row, col = [int(k, 2) for k in to_bin(d)]
     return row*8 + col
-    
+
 result = np.array(list(map(to_seat, data)))
+print(np.max(result))
+
+# part 2
 result = np.sort(result)
 missing = np.argmax(np.diff(result))
 
 print(result[missing] + 1) # seat is next missing one
+
